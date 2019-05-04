9d3993b5adeac149cc26ed962c21ed6005f0bb37
Marcin Czerny <czernymarcin@gmail.com>
2019-05-04 10:52:45 +0200

    Ciąg dalszy wprowadzania FreeRTOS

--- PPSKArdu.ino
+++ PPSKArdu.ino
@@ -58,6 +58,11 @@ int rightStop = 81;
 NewPing sonarBack(trigPinBack, echoPinBack, MAX_DISTANCE);
 NewPing sonarFront(trigPinFront,echoPinFront,MAX_DISTANCE);
 SemaphoreHandle_t xSerialSemaphore;
+byte g_ekspanderSensors;
+byte g_limitSwitchesSensors;
+unsigned int g_FrontUltraSondDistance;
+unsigned int g_RearUltraSondDistance;
+
 #pragma endregion globals
 
 #pragma region FreeRtosTasks
@@ -220,14 +225,14 @@ void on_stop(){
         timeOfLastStateSwitch = millis();
         fsm.trigger(FSM_STAIRS);
     }
-    bool obstacleDetected = false;
-    for(int i = UpLeft;i<=BackLeft;i++){
-        if(digitalRead(i)==LOW){
-            Serial.print("Wykryto zderzenie z przeszkoda na czujniku nr ");
-            Serial.println(i);
-            obstacleDetected = true;
-        }
-    }
+    // bool obstacleDetected = false;
+    // for(int i = UpLeft;i<=BackLeft;i++){
+    //     if(digitalRead(i)==LOW){
+    //         Serial.print("Wykryto zderzenie z przeszkoda na czujniku nr ");
+    //         Serial.println(i);
+    //         obstacleDetected = true;
+    //     }
+    // }
     if(obstacleDetected == true && millis() - timeOfLastStateSwitch > 50){
         timeOfLastStateSwitch = millis();
         fsm.trigger(FSM_OBSTACLE);
@@ -406,140 +411,7 @@ void setup() {
 }
 
 void loop(){
-//Lets start finite state machine
-fsm.run_machine();
-// if (!dmpReady) return;
 
-//     // wait for MPU interrupt or extra packet(s) available
-//     while (!mpuInterrupt && fifoCount < packetSize) {
-//         if (mpuInterrupt && fifoCount < packetSize) {
-//           // try to get out of the infinite loop 
-//           fifoCount = mpu.getFIFOCount();
-//         }  
-//         // other program behavior stuff here
-//         // .
-//         // .
-//         // .
-//         // if you are really paranoid you can frequently test in between other
-//         // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-//         // while() loop to immediately process the MPU data
-//         // .
-//         // .
-//         // .
-//     }
-
-//     // reset interrupt flag and get INT_STATUS byte
-//     mpuInterrupt = false;
-//     mpuIntStatus = mpu.getIntStatus();
-
-//     // get current FIFO count
-//     fifoCount = mpu.getFIFOCount();
-
-//     // check for overflow (this should never happen unless our code is too inefficient)
-//     if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
-//         // reset so we can continue cleanly
-//         mpu.resetFIFO();
-//         fifoCount = mpu.getFIFOCount();
-//         Serial.println(F("FIFO overflow!"));
-
-//     // otherwise, check for DMP data ready interrupt (this should happen frequently)
-//     } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
-//         // wait for correct available data length, should be a VERY short wait
-//         while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-//         // read a packet from FIFO
-//         mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
-//         // track FIFO count here in case there is > 1 packet available
-//         // (this lets us immediately read more without waiting for an interrupt)
-//         fifoCount -= packetSize;
-
-//         #ifdef OUTPUT_READABLE_QUATERNION
-//             // display quaternion values in easy matrix form: w x y z
-//             mpu.dmpGetQuaternion(&q, fifoBuffer);
-//             Serial.print("quat\t");
-//             Serial.print(q.w);
-//             Serial.print("\t");
-//             Serial.print(q.x);
-//             Serial.print("\t");
-//             Serial.print(q.y);
-//             Serial.print("\t");
-//             Serial.println(q.z);
-//         #endif
-
-//         #ifdef OUTPUT_READABLE_EULER
-//             // display Euler angles in degrees
-//             mpu.dmpGetQuaternion(&q, fifoBuffer);
-//             mpu.dmpGetEuler(euler, &q);
-//             Serial.print("euler\t");
-//             Serial.print(euler[0] * 180/M_PI);
-//             Serial.print("\t");
-//             Serial.print(euler[1] * 180/M_PI);
-//             Serial.print("\t");
-//             Serial.println(euler[2] * 180/M_PI);
-//         #endif
-
-//         #ifdef OUTPUT_READABLE_YAWPITCHROLL
-//             // display Euler angles in degrees
-//             mpu.dmpGetQuaternion(&q, fifoBuffer);
-//             mpu.dmpGetGravity(&gravity, &q);
-//             mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-//             Serial.print("ypr\t");
-//             Serial.print(ypr[0] * 180/M_PI);
-//             Serial.print("\t");
-//             Serial.print(ypr[1] * 180/M_PI);
-//             Serial.print("\t");
-//             Serial.println(ypr[2] * 180/M_PI);
-//         #endif
-
-//         #ifdef OUTPUT_READABLE_REALACCEL
-//             // display real acceleration, adjusted to remove gravity
-//             mpu.dmpGetQuaternion(&q, fifoBuffer);
-//             mpu.dmpGetAccel(&aa, fifoBuffer);
-//             mpu.dmpGetGravity(&gravity, &q);
-//             mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-//             Serial.print("areal\t");
-//             Serial.print(aaReal.x);
-//             Serial.print("\t");
-//             Serial.print(aaReal.y);
-//             Serial.print("\t");
-//             Serial.println(aaReal.z);
-//         #endif
-
-//         #ifdef OUTPUT_READABLE_WORLDACCEL
-//             // display initial world-frame acceleration, adjusted to remove gravity
-//             // and rotated based on known orientation from quaternion
-//             mpu.dmpGetQuaternion(&q, fifoBuffer);
-//             mpu.dmpGetAccel(&aa, fifoBuffer);
-//             mpu.dmpGetGravity(&gravity, &q);
-//             mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-//             mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-//             Serial.print("aworld\t");
-//             Serial.print(aaWorld.x);
-//             Serial.print("\t");
-//             Serial.print(aaWorld.y);
-//             Serial.print("\t");
-//             Serial.println(aaWorld.z);
-//         #endif
-    
-//         #ifdef OUTPUT_TEAPOT
-//             // display quaternion values in InvenSense Teapot demo format:
-//             teapotPacket[2] = fifoBuffer[0];
-//             teapotPacket[3] = fifoBuffer[1];
-//             teapotPacket[4] = fifoBuffer[4];
-//             teapotPacket[5] = fifoBuffer[5];
-//             teapotPacket[6] = fifoBuffer[8];
-//             teapotPacket[7] = fifoBuffer[9];
-//             teapotPacket[8] = fifoBuffer[12];
-//             teapotPacket[9] = fifoBuffer[13];
-//             Serial.write(teapotPacket, 14);
-//             teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-//         #endif
-
-//         // blink LED to indicate activity
-//         blinkState = !blinkState;
-//         digitalWrite(LED_PIN, blinkState);
-// 	}
 	
 	/*while(stopEngines == false){
     right.write(rightStop-10);
@@ -626,19 +498,17 @@ void setupMPU(){
         Serial.print(F("DMP Initialization failed (code "));
         Serial.print(devStatus);
         Serial.println(F(")"));
-		
-		 }
 
     // configure LED for output
     pinMode(LED_PIN, OUTPUT);
 }
 void restartSonar(int echo, int trig){
     pinMode(echo, OUTPUT);
-    delay(150);
+    vTaskDelay(10);
     digitalWrite(echo, LOW);
-    delay(150);
+    vTaskDelay(10);
     pinMode(echo, INPUT);
-    delay(150);
+    vTaskDelay(10);
 }
 void stopEngines(){
     right.write(rightStop);//stop signal
@@ -647,23 +517,192 @@ void stopEngines(){
 #pragma region FreeRTOSFunctions
 void TaskFSM( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
+    for(;;){
+        //Lets start finite state machine
+        fsm.run_machine();
+        if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
+        {
+            Serial.println("TaskFSM");
+            xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
+        }
+        vTaskDelay(1);
+    }
+
 }
 void TaskMTU(void *pvParameters __attribute__((unused))){
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        if (mpuInterrupt && fifoCount < packetSize) {
+          // try to get out of the infinite loop 
+          fifoCount = mpu.getFIFOCount();
+        }  
+        // other program behavior stuff here
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        fifoCount = mpu.getFIFOCount();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+//         #ifdef OUTPUT_READABLE_QUATERNION
+//             // display quaternion values in easy matrix form: w x y z
+//             mpu.dmpGetQuaternion(&q, fifoBuffer);
+//             Serial.print("quat\t");
+//             Serial.print(q.w);
+//             Serial.print("\t");
+//             Serial.print(q.x);
+//             Serial.print("\t");
+//             Serial.print(q.y);
+//             Serial.print("\t");
+//             Serial.println(q.z);
+//         #endif
+
+//         #ifdef OUTPUT_READABLE_EULER
+//             // display Euler angles in degrees
+//             mpu.dmpGetQuaternion(&q, fifoBuffer);
+//             mpu.dmpGetEuler(euler, &q);
+//             Serial.print("euler\t");
+//             Serial.print(euler[0] * 180/M_PI);
+//             Serial.print("\t");
+//             Serial.print(euler[1] * 180/M_PI);
+//             Serial.print("\t");
+//             Serial.println(euler[2] * 180/M_PI);
+//         #endif
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI);
+        #endif
+
+//         #ifdef OUTPUT_READABLE_REALACCEL
+//             // display real acceleration, adjusted to remove gravity
+//             mpu.dmpGetQuaternion(&q, fifoBuffer);
+//             mpu.dmpGetAccel(&aa, fifoBuffer);
+//             mpu.dmpGetGravity(&gravity, &q);
+//             mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+//             Serial.print("areal\t");
+//             Serial.print(aaReal.x);
+//             Serial.print("\t");
+//             Serial.print(aaReal.y);
+//             Serial.print("\t");
+//             Serial.println(aaReal.z);
+//         #endif
+
+//         #ifdef OUTPUT_READABLE_WORLDACCEL
+//             // display initial world-frame acceleration, adjusted to remove gravity
+//             // and rotated based on known orientation from quaternion
+//             mpu.dmpGetQuaternion(&q, fifoBuffer);
+//             mpu.dmpGetAccel(&aa, fifoBuffer);
+//             mpu.dmpGetGravity(&gravity, &q);
+//             mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+//             mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+//             Serial.print("aworld\t");
+//             Serial.print(aaWorld.x);
+//             Serial.print("\t");
+//             Serial.print(aaWorld.y);
+//             Serial.print("\t");
+//             Serial.println(aaWorld.z);
+//         #endif
+    
+//         #ifdef OUTPUT_TEAPOT
+//             // display quaternion values in InvenSense Teapot demo format:
+//             teapotPacket[2] = fifoBuffer[0];
+//             teapotPacket[3] = fifoBuffer[1];
+//             teapotPacket[4] = fifoBuffer[4];
+//             teapotPacket[5] = fifoBuffer[5];
+//             teapotPacket[6] = fifoBuffer[8];
+//             teapotPacket[7] = fifoBuffer[9];
+//             teapotPacket[8] = fifoBuffer[12];
+//             teapotPacket[9] = fifoBuffer[13];
+//             Serial.write(teapotPacket, 14);
+//             teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+//         #endif
+
+//         // blink LED to indicate activity
+//         blinkState = !blinkState;
+//         digitalWrite(LED_PIN, blinkState);
+// 	}
 
 }
 void TaskFrontUltasond( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
+    int uS = sonarFront.ping_cm();
+    if (uS==0)
+    {
+        resetSensors();
+    }else{
+        g_FrontUltraSondDistance = uS;
+    }
+    vTaskDelay(1);
 }
 void TaskRearUltrasond(void *pvParameters __attribute__((unused))){
-
+    int uS = sonarFront.ping_cm();
+    if (uS==0)
+    {
+        resetSensors();
+    }else{
+        g_RearUltraSondDistance = uS;
+    }
+    vTaskDelay(1);
 }
 void TaskSerialRead( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
+    vTaskDelay(1);
 }
 void TaskSwitches(void *pvParameters __attribute__((unused))){
+    //bool obstacleDetected = false;
+    g_limitSwitchesSensors = 1;
+    for(int i = UpLeft;i<=BackLeft;i++){
+        g_limitSwitchesSensors = g_limitSwitchesSensors & digitalRead(i); 
+        g_limitSwitchesSensors <<=1;
+    //    if(digitalRead(i)==LOW){
+     //       Serial.print("Wykryto zderzenie z przeszkoda na czujniku nr ");
+     //       Serial.println(i);
+    //        obstacleDetected = true;
+    //    }
 
+    }
+    
+    vTaskDelay(1);
 }
 void TaskExpander(void *pvParameters __attribute__((unused))){
+    g_ekspanderSensors = 1;
+    for(int i = 0;i<7;i++){
+        g_ekspanderSensors = g_ekspanderSensors & ekspander.digitalRead(i); 
+        g_ekspanderSensors <<=1;
+    }
 
+    vTaskDelay(1);
 }
 #pragma endregion FreeRTOSFunctions 
